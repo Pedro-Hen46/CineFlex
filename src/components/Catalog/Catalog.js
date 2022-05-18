23e2e75c8624c2c49816701a29fa21217ddb3546
Pedro <driverique@gmail.com>
2022-05-18 20:17:17 -0300

    Renderizando o catalogo disponivel

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -2,13 +2,16 @@ import '../Catalog/styles.css'
 
 export default function Catalog({catalog}){
     
+    function desenvolvimento(){
+        alert('Ainda em fase desenvolvimento');
+    }
 
     return (
         <div className='catalog'>
             <h3>Selecione o filme</h3>
             
             <div className="capas">
-                {catalog.map( item => <img className='filmeCapa' src={item.posterURL} />) }
+                {catalog.map( item => <img onClick={desenvolvimento()} className='filmeCapa' src={item.posterURL} />) }
             </div>
             
         </div>
