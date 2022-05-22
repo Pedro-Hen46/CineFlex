a2e474e1ce0add0bc37e86d7f0e7290e0ab22c0e
Pedro Henrique <driverique@gmail.com>
2022-05-22 10:20:15 -0700

    Mostrando poltronas responsivas da api, falta funcionalidade

--- src/components/Assentos/Assentos.js
+++ src/components/Assentos/Assentos.js
@@ -21,18 +21,30 @@ export default function Assentos({ id }) {
 
     }), []);
 
+    const { seats } = assento;
+
+    console.log(seats);
+
     return (
         <div className='section-info'>
             {assento === 0 ? ''
                 :
                 <div className="escolhendoAssento">
                     <h3>Selecione o(s) assento(s)</h3>
-
-                    <div className='poltronas'>
-                        
+                 
+                    <div className="poltronas">
+                        {seats.map((value, index) => 
+                            <button className={value.isAvailable === true ? 'poltrona' : 'poltrona-vermelha'} key={index}>   <h6>{value.name}</h6> </button>
+                        )}
                     </div>
 
                     <button className='button-reserv'><span>Reservar assento(s)</span></button>
+
+                    <span>Nome do comprador</span>
+                    <input placeholder="Digite o seu nome..."></input>
+                    <span>CPF do comprador</span>
+                    <input placeholder="XXX.XXX.XXX-XX"></input>
+
                     <FooterInf hora={assento.name} movie={assento.movie} day={assento.day} />
                 </div>
             }
